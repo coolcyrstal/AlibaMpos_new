d61d08bea9b75d2c51ea656d452c616f4b8f78cb
chayenjr <chayenjr.sk131@gmail.com>
2016-06-02 16:08:31 +0700

    fragment

--- app/src/main/java/com/example/chayenjr/alibampos/LoginPage.java
+++ app/src/main/java/com/example/chayenjr/alibampos/LoginPage.java
@@ -3,33 +3,22 @@ package com.example.chayenjr.alibampos;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
 public class LoginPage extends AppCompatActivity {
-    /**
-     * Whether or not the system UI should be auto-hidden after
-     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-     */
+
     private static final boolean AUTO_HIDE = true;
 
-    /**
-     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-     * user interaction before hiding the system UI.
-     */
     private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
 
-    /**
-     * Some older devices needs a small delay between UI widget updates
-     * and a change of the status and navigation bar.
-     */
     private static final int UI_ANIMATION_DELAY = 300;
     private final Handler mHideHandler = new Handler();
     private View mContentView;
@@ -37,11 +26,7 @@ public class LoginPage extends AppCompatActivity {
         @SuppressLint("InlinedApi")
         @Override
         public void run() {
-            // Delayed removal of status and navigation bar
 
-            // Note that some of these constants are new as of API 16 (Jelly Bean)
-            // and API 19 (KitKat). It is safe to use them, as they are inlined
-            // at compile-time and do nothing on earlier devices.
             mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
                     | View.SYSTEM_UI_FLAG_FULLSCREEN
                     | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
@@ -69,11 +54,7 @@ public class LoginPage extends AppCompatActivity {
             hide();
         }
     };
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
+
     private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
         @Override
         public boolean onTouch(View view, MotionEvent motionEvent) {
@@ -100,41 +81,22 @@ public class LoginPage extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 toggle();
-//
-//                Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.startpaymentPage);
-//                StartPayment myfragment = (StartPayment) fragment;
-//                FragmentManager manager = getSupportFragmentManager();
-//                FragmentTransaction transaction = manager.beginTransaction();
-//                transaction.replace(R.id.startpaymentPage, fragment);
-//                transaction.commit();
-//                try {
-//                    Thread.sleep(4000);
-//                } catch (InterruptedException e) {
-//                    e.printStackTrace();
-//                }
             }
         });
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
+
         findViewById(R.id.signin_button).setOnTouchListener(mDelayHideTouchListener);
     }
 
     public void buttonOnClick(View v){
-        Button signin = (Button)v;
-        ((Button) v).setText("success");
-//        Intent gostart = new Intent(this, StartPayment.class);
-//        startActivity(gostart);
-        getFragmentManager().beginTransaction().replace(R.id.startpaymentPage, new com.example.chayenjr.alibampos.List()).commit();
+        StartPayment fragment = new StartPayment();
+        FragmentTransaction i = getSupportFragmentManager().beginTransaction();
+        i.add(R.id.startpaymentPage, fragment);
+        i.commit();
     }
 
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
-
-        // Trigger the initial hide() shortly after the activity has been
-        // created, to briefly hint to the user that UI controls
-        // are available.
         delayedHide(100);
     }
 
@@ -147,7 +109,6 @@ public class LoginPage extends AppCompatActivity {
     }
 
     private void hide() {
-        // Hide UI first
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.hide();
@@ -172,10 +133,6 @@ public class LoginPage extends AppCompatActivity {
         mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
     }
 
-    /**
-     * Schedules a call to hide() in [delay] milliseconds, canceling any
-     * previously scheduled calls.
-     */
     private void delayedHide(int delayMillis) {
         mHideHandler.removeCallbacks(mHideRunnable);
         mHideHandler.postDelayed(mHideRunnable, delayMillis);
